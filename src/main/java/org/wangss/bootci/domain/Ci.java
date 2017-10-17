4858337501474bc68a863aeefd8173a4f3dbbde4
wangss <genhaoai@gmail.com>
2017-10-17 22:31:47 +0800

    bean信息修改

--- src/main/java/org/wangss/bootci/domain/Ci.java
+++ src/main/java/org/wangss/bootci/domain/Ci.java
@@ -11,8 +11,10 @@ public class Ci {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String author;
-    private String dynasty;
-    private String content;
-    private String picUrl;
+    private String author;//作者
+    private String dynasty;//朝代
+    private String name; //词牌名
+    private String title;//题目
+    private String content;//内容
+    private String picUrl;//图片地址
 }
--- src/main/java/org/wangss/bootci/domain/UserInfo.java
+++ src/main/java/org/wangss/bootci/domain/UserInfo.java
@@ -14,10 +14,10 @@ public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String userId;
-    private String userName;
-    private String password;
-    private Date insertTime;
-    private Boolean isAdmin;
-    private Boolean isSuperAdmin;
+    private String userId;//用户ID
+    private String userName;//用户名称
+    private String password;//用户密码
+    private Date insertTime;//新建时间
+    private Boolean isAdmin;//是否管理员
+    private Boolean isSuperAdmin;//是否超级管理员
 }
